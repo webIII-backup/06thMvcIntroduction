7fdba07ba1d6bec7bf34e9e0c7dc437f5026502f
Stefaan De Cock <Stefaan.DeCock@hogent.be>
2017-10-16 09:10:02 +0200

    Create Controller and View

---
+++ SnakeEyesGame/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SnakeEyesGame.Models;
+
+namespace SnakeEyesGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SnakeEyes _snakeEyes;
+        public IActionResult Index()
+        {
+            _snakeEyes = new SnakeEyes();
+            return View(_snakeEyes);
+        }
+    }
+}
